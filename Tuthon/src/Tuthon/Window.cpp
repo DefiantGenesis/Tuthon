48c5d1b441d013fb41dfc2142c488861fca90955
Jacob <ldsscirocks@gmail.com>
2018-11-22 08:27:01 -0700

    TRIANGLE

--- Tuthon/src/Tuthon/Window.cpp
+++ Tuthon/src/Tuthon/Window.cpp
@@ -27,6 +27,12 @@ namespace Tuthon {
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
